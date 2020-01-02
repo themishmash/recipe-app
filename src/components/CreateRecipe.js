50093bf707b2f56c2dbe05d7e9469e545e84702d
Michelle <wong.mich.c@gmail.com>
2020-01-02 21:59:15 +1100

    createrecipe updating class extends etc

--- src/components/CreateRecipe.js
+++ src/components/CreateRecipe.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Navbar from './Navbar';
 
-function CreateRecipe ()
-{
-  return (<div>
-     <div><Navbar /></div>
-    <h1>This is the create Recipe</h1>
-  </div>)
-}
+class CreateRecipe extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      peepname: '',
+      title: '',
+      url: '',
+      date: new Date(),
+      users: [] //this is array as on our page there will be drop down menu for users so can select all the users already in database
+    }
+  }
+
+  render () {
+    return (<div>
+       <div><Navbar /></div>
+      <h1>This is the create Recipe</h1>
+    </div>)
+  }
+} 
 
 
 export default CreateRecipe
